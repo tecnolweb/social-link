91b9785e59fa8bf95213b14e80f3476bbe78acca
tecnolweb <141664255+tecnolweb@users.noreply.github.com>
2023-08-12 17:36:33 -0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -6,7 +6,7 @@ const imgs = document.getElementById('imgs')
   
  let idx = 0 
   
- let interval = setInterval(run, 5000) 
+ let interval = setInterval(run, 10000) 
   
  function run() { 
      idx++ 
@@ -25,7 +25,7 @@ const imgs = document.getElementById('imgs')
   
  function resetInterval() { 
      clearInterval(interval) 
-     interval = setInterval(run, 5000) 
+     interval = setInterval(run, 10000) 
  } 
   
  rightBtn.addEventListener('click', () => { 
